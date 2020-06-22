9ac8acd29c47dd0f2c8afde22c8fb4540cd4af1e
A H <>
2020-06-22 12:00:55 -0700

    started users

---
+++ models/user.js
@@ -0,0 +1,89 @@
+/** User class for message.ly */
+
+const db = require("../db");
+const ExpressError = require("../expressError");
+
+/** User of the site. */
+
+class User {
+
+  /** register new user -- returns
+   *    {username, password, first_name, last_name, phone}
+   */
+
+  static async register({username, password, first_name, last_name, phone}) {
+    // check if user already exists
+    const user = await db.query(
+      `SELECT username
+      FROM users
+      WHERE username=$1`,
+      [username]
+    );
+      
+    if(user.rows.length) {
+      // const err = new Error("User already exists.");
+      // err.status = 404;
+      const err = new ExpressError("User already exists.", 404);
+      return next(err);
+    }
+    // insert the user into the database
+    const response = await db.query(
+      `INSERT INTO users
+        username,
+        password,
+        first_name as "firstName",
+        last_name as "lastName",
+        phone
+      VALUES ($1, $2, $3, $4, $5)`,
+      [username, password, first_name, last_name, phone]
+    );
+    
+  }
+
+  /** Authenticate: is this username/password valid? Returns boolean. */
+
+  static async authenticate(username, password) { }
+
+  /** Update last_login_at for user */
+
+  static async updateLoginTimestamp(username) { }
+
+  /** All: basic info on all users:
+   * [{username, first_name, last_name}, ...] */
+
+  static async all() { }
+
+  /** Get: get user by username
+   *
+   * returns {username,
+   *          first_name,
+   *          last_name,
+   *          phone,
+   *          join_at,
+   *          last_login_at } */
+
+  static async get(username) { }
+
+  /** Return messages from this user.
+   *
+   * [{id, to_user, body, sent_at, read_at}]
+   *
+   * where to_user is
+   *   {username, first_name, last_name, phone}
+   */
+
+  static async messagesFrom(username) { }
+
+  /** Return messages to this user.
+   *
+   * [{id, from_user, body, sent_at, read_at}]
+   *
+   * where from_user is
+   *   {id, first_name, last_name, phone}
+   */
+
+  static async messagesTo(username) { }
+}
+
+
+module.exports = User;
---
+++ routes/auth.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = new express.Router();
+
+/** POST /login - login: {username, password} => {token}
+ *
+ * Make sure to update their last-login!
+ *
+ **/
+
+
+/** POST /register - register user: registers, logs in, and returns token.
+ *
+ * {username, password, first_name, last_name, phone} => {token}.
+ *
+ *  Make sure to update their last-login!
+ */
+
+router.post("/register", (req, res, next) => {
+  console.log("please work");
+});
+
+module.export = router;
